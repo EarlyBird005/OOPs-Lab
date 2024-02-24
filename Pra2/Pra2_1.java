499a169ba3fa0fa46842e6158cdf4a2936a6e159
EarlyBird <160519935+EarlyBird005@users.noreply.github.com>
2024-02-24 23:25:14 +0530

    Update Pra2_1.java

--- Pra2/Pra2_1.java
+++ Pra2/Pra2_1.java
@@ -1,12 +1,22 @@
 /* Write a program that generate 6*6 two-dimensional matrix, filled with 0’s and 1’s , display the matrix, check every raw and column have an odd number’s of 1’s. */
 import java.util.*;
 
-class Pra2_1 {
+public class Pra2_1 {
+    static String postfix(int data) {
+        return switch (d % 10) {
+            case 1 -> (d == 11) ? "th" : "st";
+            case 2 -> (d == 12) ? "th" : "nd";
+            case 3 -> (d == 13) ? "th" : "rd";
+            default -> "th";
+        };
+    }
+
     public static void main(String[] args) {
-        Scanner se = new Scanner(System.in);
+        System.out.println("Name: Nanecha Dipak Kishorbhai\t Enrollment NO.: 220170116023");
         Date t = new Date();
         System.out.println(t);
-
+        // Scanner se = new Scanner(System.in);
+        // System.out.println("Enter 6*6 matrix with 0's & 1's: ");
         int[][] arr = new int[6][6];
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
@@ -15,7 +25,7 @@ class Pra2_1 {
             }
         }
 
-        System.out.println("6*6 Matrix:");
+        System.out.println("\n6*6 Matrix:");
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
                 System.out.print(arr[i][j] + " ");
@@ -25,30 +35,29 @@ class Pra2_1 {
 
         System.out.println("");
         for (int i = 0; i < 6; i++) {
-            int column = 0;
+            int row = 0;
             for (int j = 0; j < 6; j++) {
-                column += arr[i][j];
+                row += arr[i][j];
             }
-            if (column % 2 == 0) {
-                System.out.format("%d row has even 1's\n", i);
+            if (row % 2 == 0) {
+                System.out.println(i + postfix(i) + " row has even 1's");
             } else {
-                System.out.format("%d row has odd 1's\n", i);
+                System.out.println(i + postfix(i) + " row has odd 1's");
             }
         }
 
         System.out.println("");
         for (int i = 0; i < 6; i++) {
-            int row = 0;
+            int column = 0;
             for (int j = 0; j < 6; j++) {
-                row += arr[j][i];
+                column += arr[j][i];
             }
-            if (row % 2 == 0) {
-                System.out.format("%d column has even 1's\n", i);
+            if (column % 2 == 0) {
+                System.out.println(i + postfix(i) + " column has even 1's");
             } else {
-                System.out.format("%d column has odd 1's\n", i);
+                System.out.println(i + postfix(i) + " column has odd 1's");
             }
-
         }
-        se.close();
+        // se.close();
     }
 }
